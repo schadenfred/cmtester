b244112758b9b75503ddb8512564fa45aa21ac79
Fred Schoeneman <fred.schoeneman@gmail.com>
2011-03-29 23:51:16 -0700

    fixed route for email/input

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Cmtester::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+  
+  match 'email/input' => 'email#input', :as => :email
+  
+  
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
