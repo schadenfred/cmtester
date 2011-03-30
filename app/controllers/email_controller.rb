266588fadc6c72d5cc7ee5217ae47430e2171bc3
Fred Schoeneman <fred.schoeneman@gmail.com>
2011-03-30 14:04:10 -0700

    x

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -7,15 +7,14 @@ class EmailController < ApplicationController
     from = msg.from[0]
     from_user = User.create_or_find_by_email(from.email) # TODO: grab name from email
 
-
-    abouts = msg.to + msg.cc - ['ce1c14b80ca625c3078c@cloudmailin.net']
-    abouts.each {|user|
-      about_user = User.create_or_find_by_email(user.email) # TODO: grab name from email
-      content = Content.create(:from => from_user, :about => about_user, :subject => msg.subject, :body => msg.body)
+    toandcc = msg.to + msg.cc # - ['ce1c14b80ca625c3078c@cloudmailin.net']
+    toandcc.each {|address|
+      about_user = User.create_or_find_by_email(address.email) # TODO: grab name
+#      content = Content.create(:from => from_user, :about => about_user, :subject => msg.subject, :body => msg.body)
     
-      msg.attachments.each {|att|
-        content.attachments << Attachment.new(:filename => att.original_filename, :bits => att.body, :content_type => att.content_type)
-      }
+ #     msg.attachments.each {|att|
+ #       content.attachments << Attachment.new(:filename => att.original_filename, :bits => att.body, :content_type => att.content_type)
+ #     }
     }
   end
 end
